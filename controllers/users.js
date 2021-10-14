e7fc451baaee489ffc62aed246399da09cdc438d
Milan <milan.innovate@gmail.com>
2021-10-14 15:00:59 +0530

    error messages are added to the methods

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,65 @@
 const Users = require("../models/users");
 
 const getAllUsers = async(req, res) => {
-    const allUsers = await Users.find({});
-    res.send({msg: "get all users", data: allUsers, status: 200})
+    try
+    {
+        const allUsers = await Users.find({});
+        res.send({msg: "Successfully got all users!", data: allUsers, status: 200})
+    } catch(e){
+        console.log(e)
+        res.send({msg: e.message, status: 400})
+    }
 }
 
-
 const getUserById = async(req, res) => {
-    const {id} = req.params;
-    const userById = await Users.findById(id)
-    res.send({msg: "get user by id", data: userById, status: 200})
+    try
+    {
+        const {id} = req.params;
+        const userById = await Users.findById(id)
+        res.send({msg: "Successfully got user data", data: userById, status: 200})
+    } catch(e){
+        console.log(e)
+        res.send({msg: e.message, status: 400})
+                }       
 }
 
 const addUser = async(req, res) => {
-    const {userType, email, password, userName, phoneNumber, address} = req.body;
-    let newUser = {userType: userType, email: email, password: password, userName: userName, phoneNumber: phoneNumber, address: address}
-    const addUser = await Users.create(newUser);
-    res.send({msg: "user added", data: addUser, status: 200});
+    try
+    {
+        const {userType, email, password, userName, phoneNumber, address} = req.body;
+        let newUser = {userType: userType, email: email, password: password, userName: userName, phoneNumber: phoneNumber, address: address}
+        const addUser = await Users.create(newUser);
+        res.send({msg: "New user added succesfully!", data: addUser, status: 200});
+    } catch(e) {
+        console.log(e)
+        res.send({msg: e.message, status: 400})
+            }
+}
+
+const updateUser = async(req, res) => {
+    try
+    {
+        const {id} = req.params;
+        const updateById = await Users.findByIdAndUpdate(id, req.body, {runValidator: true, new: true})
+        res.send({msg: "User updated succesfully!", data: updateById, status: 200})
+    } catch(e){
+        console.log(e)
+        res.send({msg: e.message, status: 400})
+        }
+}
+
+const deleteUser = async(req, res) => {
+    try
+    {
+        const {id} = req.params;
+        const deleteById = await Users.findByIdAndDelete(id)
+        res.send({msg: "User deleted successfully!", data: deleteById, status: 200})
+    } catch(e){
+        console.log(e)
+        res.send({msg: e.message, status: 400})
+        }
 }
 
 module.exports = {
-    getAllUsers, getUserById, addUser
+    getAllUsers, getUserById, addUser, updateUser, deleteUser
 };
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {getAllUsers, getUserById, addUser} = require("../controllers/users");
+const {getAllUsers, getUserById, addUser, updateUser, deleteUser} = require("../controllers/users");
 
 router.get("/", getAllUsers);
 router.get("/:id", getUserById);
-
 router.post("/add", addUser);
-
+router.put("/update/:id", updateUser);
+router.delete("/delete/:id", deleteUser);
 
 module.exports = router;
