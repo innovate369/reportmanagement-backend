b0726b83b446b113d5d28e02b646bb56c2ad2870
Milan <milan.innovate@gmail.com>
2021-11-16 12:34:07 +0530

    testing husky package

--- controllers/clients.js
+++ controllers/clients.js
@@ -1,63 +1,83 @@
-const Clients = require("../models/clients");
+const Clients = require('../models/clients');
 
 const getAllClients = async (req, res) => {
-    try {
-        const { page, size = 5 } = req.query;
-        const limit = parseInt(size);
-        const skip = (page - 1) * size;
-        const search = req.query.search;
-        const result = await Clients.find({
-            $or: [{ businessCategory: { $regex: search, $options: 'i' } }, { companyName: { $regex: search, $options: 'i' } }, 
-            { ledgerName: { $regex: search, $options: 'i' } }]}).limit(limit).skip(skip).sort('createdOn');;
-            res.send({ msg: "Got all Clients succuessfully!", data: result, status: 200 });
-
-    } catch (e) {
-        res.send({ msg: e.message, status: 400 })
-    }
-}
+  try {
+    const { page, size = 5 } = req.query;
+    const limit = parseInt(size, 10);
+    const skip = (page - 1) * size;
+    const { search } = req.query;
+    const result = await Clients.find({
+      $or: [{ businessCategory: { $regex: search, $options: 'i' } }, { companyName: { $regex: search, $options: 'i' } },
+      { ledgerName: { $regex: search, $options: 'i' } }],
+    }).limit(limit).skip(skip).sort('createdOn');
+    res.send({ msg: 'Got all Clients succuessfully!', data: result, status: 200 });
+  } catch (e) {
+    res.send({ msg: e.message, status: 400 });
+  }
+};
 
 const addClient = async (req, res) => {
-        const { businessCategory, companyName, ledgerName, mobileNum, contactNum, address, countryName, stateName, cityName, pinCode, projectId } = req.body;
-        let newClient = { businessCategory, companyName, ledgerName, mobileNum, contactNum, address, countryName, stateName, cityName, pinCode, projectId }
-        const addClient = await Clients.create(newClient);
-        res.send({ msg: "New Client added succesfully!", data: addClient, status: 200 });
-    } 
+  const {
+    businessCategory, companyName, ledgerName, mobileNum, contactNum,
+    address, countryName, stateName, cityName, pinCode, projectId,
+  } = req.body;
+  const newClient = {
+    businessCategory,
+    companyName,
+    ledgerName,
+    mobileNum,
+    contactNum,
+    address,
+    countryName,
+    stateName,
+    cityName,
+    pinCode,
+    projectId,
+  };
+  const addNewClient = await Clients.create(newClient);
+  res.send({ msg: 'New Client added succesfully!', data: addNewClient, status: 200 });
+};
 
 const deleteClient = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deleteById = await Clients.findByIdAndDelete(id)
-        res.send({ msg: "Client data deleted successfully!", data: deleteById, status: 200 })
-    } catch (e) {
-        res.send({ msg: e.message, status: 400 })
-    }
-}
+  try {
+    const { id } = req.params;
+    const deleteById = await Clients.findByIdAndDelete(id);
+    res.send({ msg: 'Client data deleted successfully!', data: deleteById, status: 200 });
+  } catch (e) {
+    res.send({ msg: e.message, status: 400 });
+  }
+};
 
-const clientInvoice = async(req, res) => {
-    try{
-        const user = await Clients.findOne({ clientId: req.query.clientId }); 
-        if (user) {
-            user.payment = numWeeks*numDays*dayHours*hourRate;
-            console.log(typeof(numDays))
-            res.send(user.payment)
-        }
-    } catch {
-        res.send({ msg: e.message, status: 400 });
+const clientInvoice = async (req, res) => {
+  try {
+    const user = await Clients.findOne({ clientId: req.query.clientId });
+    const {
+      numWeeks, numDays, dayHours, hourRate,
+    } = req.body;
+    if (user) {
+      user.payment = numWeeks * numDays * dayHours * hourRate;
+      res.send(user.payment);
     }
-}
+  } catch (e) {
+    res.send({ msg: e.message, status: 400 });
+  }
+};
 
 const bindProject = async (req, res) => {
-    try {
-        const {clientId} = req.query;
-        const {newProjectId} = req.body;
-        const bindingClient = await Clients.findByIdAndUpdate(clientId, {projectId: newProjectId}, { runValidator: true, new: true }) 
-        res.send({ msg: "Client linked with new project!", data: bindingClient, status: 200 });
-    } catch (error) {
-        res.send({ msg: error.message, status: 400 });
-    }
-}
-
+  try {
+    const { clientId } = req.query;
+    const { newProjectId } = req.body;
+    const bindingClient = await Clients.findByIdAndUpdate(
+      clientId,
+      { projectId: newProjectId },
+      { runValidator: true, new: true },
+    );
+    res.send({ msg: 'Client linked with new project!', data: bindingClient, status: 200 });
+  } catch (error) {
+    res.send({ msg: error.message, status: 400 });
+  }
+};
 
 module.exports = {
-    getAllClients, addClient, deleteClient, clientInvoice, bindProject
+  getAllClients, addClient, deleteClient, clientInvoice, bindProject,
 };
